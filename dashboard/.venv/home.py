48cf910c7ebadc730ff8fe790548b389dea25a8d
Seah Jia Jun <orangepufflejj@gmail.com>
2023-09-22 17:19:19 +0800

    changed curr date mark to lighter red, dotted

--- dashboard/.venv/home.py
+++ dashboard/.venv/home.py
@@ -265,8 +265,8 @@ if st.session_state["authentication_status"]:
     )
     curr_date = alt.Chart(pd.DataFrame({
     'date': [selected_date],
-    'laterality': ['red']
-    })).mark_rule().encode(
+    'laterality': ['#ff3333']
+    })).mark_rule(strokeDash=[6,6]).encode(
     x='date:T',
     color=alt.Color('laterality:N', scale=None)
     )
