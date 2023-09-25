7db08b3ef10a1edfe6f84330ad1e4a8b0d15792b
magiciansz <80561550+magiciansz@users.noreply.github.com>
2023-09-25 22:43:04 +0800

    add cors for all routes

--- backend/appconfig.js
+++ backend/appconfig.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const { jwtStrategy } = require("./config/passport");
-
+const cors = require("cors");
 const app = express();
 
 //Loading Routes
@@ -14,6 +14,12 @@ const webRoutes = require("./routes/web");
 const errorController = require("./app/controllers/ErrorController");
 
 env.config();
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "PUT", "PATCH", "POST", "DELETE"],
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
