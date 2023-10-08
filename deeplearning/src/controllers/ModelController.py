a096337eea6acf188a772dff638a6c304ed7662c
glennbjorn <77476786+glennbjorn@users.noreply.github.com>
2023-10-08 21:35:07 +0800

    Updated requirements txt file

--- deeplearning/src/controllers/ModelController.py
+++ deeplearning/src/controllers/ModelController.py
@@ -10,22 +10,26 @@ from wtforms import ValidationError
 
 from ..util import parse_params
 
+import torch
+from torch import nn
+from torchvision import transforms as T, models
+import cv2 as cv
+from PIL import Image
 
 class ModelController(Resource):
     def post(self):
         """ Create an user based on the sent information """
-        left_eye_image = request.files['left_eye_image']
-        right_eye_image = request.files['right_eye_image']
-        if not left_eye_image or not right_eye_image:
-            abort(400, description = 'Please enter a left_eye_image or right_eye_image')
-        if not left_eye_image.mimetype.startswith('image/'):
-            abort(400, description = 'The left_eye_image has to be an image of jpeg or png.')
-        if not right_eye_image.mimetype.startswith('image/'):
+        image = request.files['image']
+
+        if not image:
+            abort(400, description = 'Please enter an  image')
+
+        if not image.mimetype.startswith('image/'):
             abort(400, description = 'The right_eye_image has to be an image of jpeg or png.')
         
 
         # process left_eye_image and right_eye_image here; variables below are the binaries of the images
-        left_eye_image_binary = left_eye_image.read()
-        right_eye_image_binary = right_eye_image.read()
-        return jsonify({"left_glaucoma_prob": left_eye_image.mimetype})
+        # left_eye_image_binary = left_eye_image.read()
+        # right_eye_image_binary = right_eye_image.read()
+        # return jsonify({"left_glaucoma_prob": left_eye_image.mimetype})
 
