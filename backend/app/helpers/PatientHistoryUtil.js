f21c8b0c6442a3f589291b3206807aa2fe827ffc
magiciansz <80561550+magiciansz@users.noreply.github.com>
2023-10-09 13:04:32 +0800

    fix error

--- backend/app/helpers/PatientHistoryUtil.js
+++ backend/app/helpers/PatientHistoryUtil.js
@@ -7,17 +7,11 @@ const convertPatientHistoryFormat = (histories, timezone) => {
   histories.forEach((history) => {
     history.visit_date = formatDateTime(moment(history.visit_date), timezone);
     if (Object.prototype.hasOwnProperty.call(patientHistory, history.id)) {
-      const currentDate = new Date(history.visit_date);
-      const previousDate = new Date(
-        patientHistory[history.id][
-          patientHistory[history.id].length - 1
-        ].visit_date
-      );
+      const previousDate =
+        patientHistory[history.id][patientHistory[history.id].length - 1]
+          .visit_date;
       // compare patientHistory with the previous one. if it is the same date, we pop the latest one
-      if (
-        currentDate.toISOString().split("T")[0] !=
-        previousDate.toISOString().split("T")[0]
-      ) {
+      if (history.visit_date.split("T")[0] != previousDate.split("T")[0]) {
         patientHistory[history.id].push(history);
       }
     } else {
